b6a7280820d21569a863697ee4dc5e9e2c98ef56
domster4444 <kshitiz.shah@deerwalk.edu.np>
2022-03-18 06:41:18 -0400

    Fix(nav): remove unnecessary space contained by hamburger

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -19,7 +19,7 @@
 <div id="featured-blog-card">
 <div class="featured-blog-card__img-container">
 
-    <img src="https://media.istockphoto.com/photos/bloggingblog-concepts-ideas-with-worktable-picture-id922745190?k=20&m=922745190&s=612x612&w=0&h=TqsA7NeMPYXmK1TY5dsbdIsczaUK0OgguehHWdSUqL8=" alt="" />
+    <img src="https://laxmanbaralblog.com/wp-content/uploads/2021/01/blogging-1.jpg" alt="" />
 </div>                   
                     <h4>january 1,2021</h4>
                     <h2>Featured Post Title</h2>
