87b0ba27336740df13d099aeeb1359ba1880bf1a
domster4444 <kshitiz.shah@deerwalk.edu.np>
2022-03-19 13:42:56 -0400

    Feat(ui): add video section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,6 +56,53 @@
 
 </main>
 
+
+
+
+
+<!-- video section start -->
+<div class="containerCenter">
+
+  <div class="contentBlock video_section-container">
+
+
+
+    <section class="video_section">
+
+      <div class="left-division">
+
+
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/OQF8WHbOFQU" title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen></iframe>
+        {{--
+        <iframe width="500" height="415" src="https://www.youtube.com/watch?v=MUddtVKe3qQ">
+        </iframe> --}}
+
+
+      </div>
+      <div class="right-division">
+        <u>
+          Check out our video
+        </u>
+        <h3 class="source_700">
+          Watch our video
+        </h3>
+        <button>Watch Now</button>
+
+      </div>
+
+
+
+    </section>
+
+  </div>
+</div>
+<!-- video section end -->
+
+
+
 <!-- subscribe to news lettersecction start herer -->
 <div class="containerCenter">
 
@@ -92,42 +139,6 @@
 <!-- subscribe to news lettersecction start herer -->
 
 
-<!-- video section start -->
-<div class="containerCenter">
-
-  <div class="contentBlock">
-
-
-
-    <section class="video_section">
-
-      <div class="left-division">
-
-        <iframe width="500" height="415"
-          src="https://rr1---sn-h5576nsl.c.drive.google.com/videoplayback?expire=1647618417&ei=MXE0YuClB5OArvIP0OakwAE&ip=103.225.244.158&cp=QVRJWUdfV1RPSFhPOjZDblpCamdINjBuRzJIbC1fQ1JSdEtEODNSVnhfUWcxdVpYSi12T0UyMGg&id=625945077dbe2725&itag=18&source=webdrive&requiressl=yes&mh=cG&mm=32&mn=sn-h5576nsl&ms=su&mv=m&mvi=1&pl=24&ttl=transient&susc=dr&driveid=15VNqk9NP2icRvJ68sBcDCYIV3meD5VDa&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=180.628&lmt=1641220956664908&mt=1647603622&subapp=DRIVE_WEB_FILE_VIEWER&txp=0011224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRQIhAPvabIQg4lpBLpKE7sDtTcjWUFTicJBcXcZPmJzLyE67AiBdISh9NUO-cuXr7OQmpaRwUBTpGjYi0tmcUOYNaWB_Nw==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhAKdFyxNzOmwoO1KdzVANVuni4gAFVnb-mllc-_JJGT9YAiEAwm_5dBNax-0z3AF3L8AoQVW0sLVvS6vaV_baIFu0Cpo=&cpn=8cU-amh4d1btrF_T&c=WEB_EMBEDDED_PLAYER&cver=1.20220316.01.00">
-        </iframe>
-
-
-      </div>
-      <div class="right-division">
-        <u>
-          Check out our video
-        </u>
-        <h3 class="source_700">
-          Watch our video
-        </h3>
-        <button>Watch Now</button>
-
-      </div>
-
-
-
-    </section>
-
-  </div>
-</div>
-<!-- video section end -->
-
 
 
 @endsection
@@ -139,17 +150,16 @@
 
 
 @section('scripts')
-<<<<<<< HEAD
-<!-- **vanta cdn starts here  -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r120/three.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vanta/0.5.21/vanta.birds.min.js"
-  integrity="sha512-bP14TXFcd8A4oGg1S37dzXTOSUiNl6/6e2sPbkUzuPgU4lDAcrKLIs8mKLfp6OJg4K+EPn5oL6CVd2wKtuNcrg=="
-  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<!-- **vanta cdn ends here  -->
+<<<<<<< HEAD <!-- **vanta cdn starts here -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r120/three.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/vanta/0.5.21/vanta.birds.min.js"
+    integrity="sha512-bP14TXFcd8A4oGg1S37dzXTOSUiNl6/6e2sPbkUzuPgU4lDAcrKLIs8mKLfp6OJg4K+EPn5oL6CVd2wKtuNcrg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <!-- **vanta cdn ends here  -->
 
-<!-- ?vanta js config script start here  -->
-<script type="text/javascript">
-  VANTA.BIRDS({
+  <!-- ?vanta js config script start here  -->
+  <script type="text/javascript">
+    VANTA.BIRDS({
       el: '#vanta-canvas',
       mouseControls: true,
       touchControls: true,
@@ -165,11 +175,11 @@
       cohesion: 94.0,
       quantity: 3.0,
     });
-</script>
-<!-- ?vanta js config script start ends here  -->
+  </script>
+  <!-- ?vanta js config script start ends here  -->
 
-<!-- carousel cdn import -->
-<!-- <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script> -->
-=======
->>>>>>> 3efa77d39afad408ee3eb75de96db76ddcbfd101
-@endsection
+  <!-- carousel cdn import -->
+  <!-- <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script> -->
+  =======
+  >>>>>>> 3efa77d39afad408ee3eb75de96db76ddcbfd101
+  @endsection
